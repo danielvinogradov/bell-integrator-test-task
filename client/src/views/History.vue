7756128516b1668ab7239573e03654920dd12422
Daniel Vinogradov <46866852+danielvinogradov@users.noreply.github.com>
2021-01-14 02:57:18 +0300

    Устанавливает boilerplate для vue приложения. Удаляет лишние файлы. Устанавливает и фиксирует зависимости.

---
+++ client/src/views/History.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+  section.history
+</template>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div.home
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class Home extends Vue {
+}
+</script>
